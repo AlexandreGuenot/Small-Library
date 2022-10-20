a98e7319fcd08eeed02b84b2093ca07d37b3a2e4
Alex Guenot <aguenot75@gmail.com>
2022-10-20 20:37:38 +0200

    Header ok

--- script.js
+++
@@ -1,9 +0,0 @@
-const library = document.querySelector('.booksContainer')
-function displayBooks(numberofBooks) {
-  for (let  i = 0; i < numberofBooks; i++) {
-      let bookCard = document.createElement('div')
-      bookCard.classList.add('card')
-      library.appendChild(bookCard)
-    }
-}
-//displayBooks(20)
